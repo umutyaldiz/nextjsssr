666234da88a4458eb96c92226d7d22b770452a3b
umutyaldiz <umutyaldiz95@gmail.com>
2022-01-17 21:18:38 +0300

    progress bar added ++

--- layouts/LayoutCategory.js
+++ layouts/LayoutCategory.js
@@ -1,9 +1,10 @@
-import ProgressBar from "@/components/ProgressBar";
+import NextNProgress from "nextjs-progressbar";
 import HeaderCategory from "@/components/Header/HeaderCategory";
 
 const LayoutCategory = ({ children }) => {
     return (
         <div className="master sports">
+            <NextNProgress height={2} color="#ffffff" />
             <HeaderCategory />
             {children}
         </div>
--- layouts/index.js
+++ layouts/index.js
@@ -1,9 +1,10 @@
-import ProgressBar from "@/components/ProgressBar";
+import NextNProgress from "nextjs-progressbar";
 import Header from "@/components/Header";
 
 const LayoutMaster = ({ children }) => {
     return (
         <div className="master">
+            <NextNProgress height={2} color="#cc0000" />
             <Header />
             {children}
         </div>
