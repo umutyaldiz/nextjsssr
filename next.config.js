8421242c60da5808f935da41291d826f1ff90509
umutyaldiz <umutyaldiz95@gmail.com>
2022-01-19 04:00:29 +0300

    CDN EXAMPLE

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   reactStrictMode: true,
-  //assetPrefix: isProd ? 'https://cdn.mydomain.com' : '',  //CDN example,
+  assetPrefix: isProd ? 'https://umutyaldiz.com/cdn' : '',  //CDN example,
   env: {
     DefaultSeo: {
       title: 'Sitename',
